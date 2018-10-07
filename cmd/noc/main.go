30c6dcb4a472e0c003f85512f23515031c49dca6
keks <keks@cryptoscope.co>
2018-10-07 02:15:06 +0200

    add noc command

---
+++ cmd/noc/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"go.cryptoscope.co/nocomment"
+)
+
+func die(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
+
+func check(err error, f func(error)) {
+	if err != nil {
+		f(err)
+	}
+}
+
+func main() {
+	r := nocomment.NewReader(os.Stdin)
+	_, err := io.Copy(os.Stdout, r)
+	check(err, die)
+}
